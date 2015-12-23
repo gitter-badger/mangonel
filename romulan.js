41661d47a13cd7563818696be8bc3335e1292157
Simon W. Jackson <simon@simonwjackson.com>
2015-12-23 15:04:31 +0100

    Cleanup the loading of the default config file

--- romulan.js
+++ romulan.js
@@ -5,48 +5,39 @@ var util = require('util');
 var cp = require("child_process");
 
 var romulanDefaults = {
-  'filename': "defaults.json"
+  'filename': "./defaults.json"
 };
 
 module.exports = function(obj) {
-  return fs.readFileAsync(romulanDefaults['filename']).then(function(content) {
-    romulanDefaults['content'] = content;
-    return Promise.resolve(content);
-  })
-  .then(function(content) {
-    romulanDefaults['json'] = JSON.parse(content);
-    return Promise.resolve();
-  })
-  .then(function() {
-    settings = Object.assign(romulanDefaults['json'], obj);
-    return Promise.resolve(settings);
-  })
-  .then(function(settings) {
-      var missing = _.isEmpty(settings.launcher.path) ||
-          _.isEmpty(settings.launcher.name) ||
-          _.isEmpty(settings.app);
+    return new Promise(function(resolve, reject) {
+        romulanDefaults['json'] = require(romulanDefaults['filename']);
+        settings = Object.assign(romulanDefaults['json'], obj);
 
-      if (missing) {
+        var missing = _.isEmpty(settings.launcher.path) ||
+              _.isEmpty(settings.launcher.name) ||
+              _.isEmpty(settings.app);
+
+        if (missing) {
           throw "Missing information";
-      }
+        }
 
-      command = buildCommand(settings);
-      cp.exec(command);
+        command = buildCommand(settings);
+        cp.exec(command);
 
-      return Promise.resolve(cp);
-  });
+        return resolve(cp);
+    });
 };
 
 function buildCommand(settings) {
-      var command = settings.launcher.path + ' ' + settings.app;
+    var command = settings.launcher.path + ' ' + settings.app;
 
-      if (process.platform == "linux") {
+    if (process.platform == "linux") {
 
-      }
+    }
 
-      else if (process.platform == "windows") {
-          command = 'start ' + command;
-      }
+    else if (process.platform == "windows") {
+      command = 'start ' + command;
+    }
 
-      return command;
+    return command;
 }
